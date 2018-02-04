5efda25965c45eebaab324341577ce468be98e8e
Tyler <tbuchhei@alumni.nd.edu>
2018-02-03 18:37:34 -0600

    Implement basic handlebars templates

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to this test site!'
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
